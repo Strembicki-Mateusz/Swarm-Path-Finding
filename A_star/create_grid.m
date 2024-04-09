ea7030cc9a35143e1ffef44e445d0f4e39d90c07
Strembicki-Mateusz <117041712+Strembicki-Mateusz@users.noreply.github.com>
2024-04-09 13:44:51 +0200

    Change the number of obstacles

--- A_star/create_grid.m
+++ A_star/create_grid.m
@@ -11,17 +11,19 @@ y_end = end_points(2,1);
 grid = 5*ones(rows,columns);
 
 for i = 1:length(N)
-    grid(x_start,y_start) = 1;
+    grid(x_start,y_start) = -2;
     grid(x_end,y_end) = 0;
 end
 
 % Set obstacles
 obstacles = ceil((rows*(columns-2)/randi([2 4])) + mod(rows*(columns-2)/randi([3 columns-2]),3));
 
-for i = 1:ceil(obstacles*3/2)
+for i = 1:ceil(obstacles*2/1.5)
     x = randi([1 rows]);
     y = randi([2 columns-1]);
     if grid(x,y) ~= -1
         grid(x,y) = -1;
     end
+end
+
 end
