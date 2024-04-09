535716db3a7fcd32209b45335d01d33b01239fb6
Strembicki-Mateusz <117041712+Strembicki-Mateusz@users.noreply.github.com>
2024-04-09 14:16:41 +0200

    Add Contents file to make documentation of code

---
+++ A_star/Contents.m
@@ -0,0 +1,9 @@
+% Swarm_Inteligance - main function that you should run.
+% link_grids - combain grids
+% insert_open - fill OPEN list 
+% heuristic - calculate heuristics
+% grid_weights - calculate weights in grid
+% distance - calculate Manchatan norm
+% create_grid - create space to do somethink
+% array - create helpful array
+% a_star - The Heart of this program ;)
