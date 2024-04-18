63b1f6c277db5390153f1ec25f2669a4f0b96882
Strembicki-Mateusz <117041712+Strembicki-Mateusz@users.noreply.github.com>
2024-04-18 11:31:16 +0200

    updating file to working version with swarm

--- A_star/Swarm_Inteligance.m
+++ A_star/Swarm_Inteligance.m
@@ -9,35 +9,73 @@ clc;
 % Target = 0;
 
 % Number of Robots
-N = 1;
+N = 3;
 
-% Grid size
-Rows = 8;
-Columns = 6;
+% Grid size (Minimum size of column is 5)
+Rows = 20;
+Columns = 15;
 
-% Start & end point
-x_start = randi([1 Rows]);
-y_start = 1;
-x_end = randi([1 Rows]);
+x_start = [];
+y_start = [];
+buffer_start = [];
+x_end = [];
+y_end = [];
+buffer_end = [];
+
+
 y_end = Columns;
+
+Start = [];
+End = [];
+% Start & end point
+for i = 1:N
+    x_start(:,i) = randi([1 Rows]);
+    buffer_start(:,1) = x_start(:,i);
+    y_start(:,i) = 1;
+    for j = 1:size(buffer_start)
+        if buffer_start(:,j) == x_start(:,j)
+           i = i - 1;
+        end
+    end
+end
+for i = 1:N
+    x_end(:,i) = randi([1 Rows]);
+    buffer_end(:,1) = x_end(:,i);
+    y_end(:,i) = Columns;
+    for j = 1:size(buffer_end)
+        if buffer_end(:,j) == x_end(:,j)
+           i = i - 1;
+        end
+    end
+end
 
 Start = [x_start; y_start];
 End = [x_end; y_end];
 
-% Plot the space
-Grid = create_grid(Rows, Columns, Start, End, N);
-Grid_With_Weights = grid_weights(Rows, Columns, End);
 
+% x_start = randi([1 Rows]);
+% y_start = 1;
+% x_end = randi([1 Rows]);
+% y_end = Columns;
+
+
+%kolor = ['ro', 'bo', 'go', 'bo', 'm0'];
+%figure(1); hold on;
+
+% Plot the space
+Grid = create_grid(Rows, Columns, Start, End, N)
+Grid_With_Weights = grid_weights(Rows, Columns, End);
 Grid = link_grids(Rows, Columns, Grid, Grid_With_Weights);
             
 
-
 % START algorithm
-Path = a_star(Rows, Columns, Grid, End, Start);
-Path(1,:)
-size(Path,1)
-Grid = -1*ones(Rows,Columns);
-for i = 1:size(Path,1)
-    Grid(Path(i,1), Path(i,2)) = i;
+for i = 1:N
+    Path = a_star(Rows, Columns, Grid, End(:,i), Start(:,i));
+    k = size(Path,1);
+    for j = 1:size(Path,1)
+        Grid(Path(j,1), Path(j,2)) = k;
+        k = k-1;
+    end
 end
 
+Grid
