c8514f6811f8f6fdfb2754a55be250a4ca847b39
Strembicki-Mateusz <117041712+Strembicki-Mateusz@users.noreply.github.com>
2024-10-18 17:26:15 +0200

    Delete A_star directory

--- A_star/node_idx.m
+++
@@ -1,9 +0,0 @@
-function n_idx = node_idx(OPEN, NODE)
-% node_idx - function which give number of node indeks
-
-    i = 1;
-    while( OPEN(i,2) ~= NODE(1) || OPEN(i,3) ~= NODE(2) )
-        i = i + 1;
-    end
-    n_idx = i;
-end
